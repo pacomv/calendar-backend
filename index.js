88bbba7f801524d23fc654566d2e36115ed8af0b
Paco <pacomorcillovizuete@gmail.com>
2023-06-25 19:55:47 +0200

    Update routes

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 // listen to requests
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}...`);
